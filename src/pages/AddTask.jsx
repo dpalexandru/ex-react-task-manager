30d5322e0ef69db15b9bb51c5fea17ca7bf9b64a
dpalexandru <alexpoian91@gmail.com>
2025-11-24 13:04:57 +0530

    [Mailstone5] aggiunto campi input alla pagina

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 
 const AddTask = () => {
+  //stati e ref
+  const [nomeTask, setNomeTask] = useState("")
+  const textareaRef = useRef();
+  const statusRef = useRef();
+
+
+  function stampaForm(e) {
+    e.preventDefault();
+    console.log(nomeTask, textareaRef.current.value, statusRef.current.value)
+  }
+
+
   return (
-    <div>AddTask</div>
+    <div className='container-form'>
+      <form action="" onSubmit={stampaForm}>
+
+        <input type="text"
+          placeholder='Neme della task'
+          value={nomeTask}
+          onChange={(e) => setNomeTask(e.target.value)}
+        />
+
+
+        <textarea name="description"
+          placeholder='Inserisci una descrizione '
+          ref={textareaRef}
+        ></textarea>
+
+
+        <select name="status" ref={statusRef}>
+          <option value="To do">To do</option>
+          <option value="Doing">Doing</option>
+          <option value="Done">Done</option>
+
+        </select>
+
+        <button type='submit'>Registra Task</button>
+
+      </form>
+
+
+    </div>
   )
 }
 
