b7002dd2ca3cb5ddc4e2f4ca3615fc170e5146ae
dpalexandru <alexpoian91@gmail.com>
2025-11-27 12:09:15 +0530

    [Mailstone11] aggiunto ordinamento per nome alfabetico task

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { useState } from 'react';
 import { useGlobalContext } from '../context/GlobalContext'
 import TaskRow from '../components/TaskRow';
 
 
 const TaskList = () => {
   const { tasks } = useGlobalContext()
+
+  //stati per l'ordinamento 
+  const [sortBy, setSortBy] = useState("createdAt")
+  const [sortOrder, setSortOrder] = useState(1)
+
+
+  //funzione ordinamento 
+  const handlerSort = (field) => {
+    if (sortBy === field) {
+      setSortOrder(prev => prev * -1)
+    } else {
+      setSortBy(field)
+      setSortOrder(1)
+    }
+  }
+
+  const sortIcon = sortOrder === 1 ? "↑" : "↓";
+
+  // array task filtrato con use memo 
+
+  const sotedTask = useMemo(() => {
+    return tasks.sort((a, b) => {
+
+      let comparison;
+
+      if (sortBy === "title") {
+        comparison = a.title.localeCompare(b.title)
+      }
+
+      return comparison * sortOrder
+
+    })
+
+  }, [tasks, sortBy, sortOrder]
+  )
+
+
   return (
     <div>
       <h1 className="titolo">Elenco dei Task</h1>
@@ -12,14 +50,28 @@ const TaskList = () => {
       <table className="task-table">
         <thead>
           <tr>
-            <th>Nome</th>
-            <th>Stato</th>
-            <th>Data di Creazione</th>
+            <th
+              onClick={() => handlerSort("title")}
+            >Nome
+              {sortBy === "title" ? sortIcon : ""}
+            </th>
+            <th
+              onClick={() => handlerSort("status")}
+
+            >Stato
+              {sortBy === "status" ? sortIcon : ""}
+            </th>
+            <th
+              onClick={() => handlerSort("createdAt")}
+
+            >Data di Creazione
+              {sortBy === "createdAt" ? sortIcon : ""}
+            </th>
           </tr>
         </thead>
 
         <tbody>
-          {tasks.map((task, index) => (
+          {sotedTask.map((task, index) => (
             <TaskRow key={index} task={task} />
           ))}
         </tbody>
